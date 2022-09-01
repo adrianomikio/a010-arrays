19214684c96b26a82eca55d4fb0920b1fa5def36
adrianomikio <adrianouge@gmail.com>
2022-09-01 12:05:22 -0300

    Exercício 2 feito

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,7 @@
 const arrayDeNumeros = [1, 5, 2, 3, 8]
 const arrayDeStrings = ["trigo", "feijão", "semente de canola"]
+const arrayDeNumerosStringsBooleanos = [1, 3, "glúten", "pimenta", 1 === "1", "dois" === `dois`]
+
+console.log(`${arrayDeNumeros}
+${arrayDeStrings}
+${arrayDeNumerosStringsBooleanos}`)
